50a73b01ba0e4556417896615ce2dbbc4c46d000
Manish Patel <maneeshkurmii@gmail.com>
2025-06-23 13:33:41 +0530

    adding token

---
+++ Frontend Test Submission/src/middleware/logger.js
@@ -0,0 +1,26 @@
+const LOG_API_URL = "http://20.244.56.144/evaluation-service/logs";
+const BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyMjAzMDUxMDUwNzI5QHBhcnVsdW5pdmVyc2l0eS5hYy5pbiIsImV4cCI6MTc1MDY2NTQwMywiaWF0IjoxNzUwNjY0NTAzLCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiODE3ZmY5NGMtMjcyMS00MGY5LThmNDQtNjNkYTUwZjNmMzM4IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoibWFuaXNoIGt1bWFyIHBhdGVsIiwic3ViIjoiYjRhZmIxN2MtMGU1YS00NzAzLTg5ODAtMmZhNjkyMTVmZDlhIn0sImVtYWlsIjoiMjIwMzA1MTA1MDcyOUBwYXJ1bHVuaXZlcnNpdHkuYWMuaW4iLCJuYW1lIjoibWFuaXNoIGt1bWFyIHBhdGVsIiwicm9sbE5vIjoiMjIwMzA1MTA1MDcyOSIsImFjY2Vzc0NvZGUiOiJUUnpnV00iLCJjbGllbnRJRCI6ImI0YWZiMTdjLTBlNWEtNDcwMy04OTgwLTJmYTY5MjE1ZmQ5YSIsImNsaWVudFNlY3JldCI6IkFHekVSTUhDUUNTdUZVTXMifQ.U_bJRAnAxG7Asi2xciozHzNqJbRTEYO67QismZ3Uxyo"; 
+
+export const log = async (stack, level, pkg, message) => {
+  const payload = {
+    stack: stack.toLowerCase(),
+    level: level.toLowerCase(),
+    package: pkg.toLowerCase(),
+    message,
+  };
+
+  try {
+    const res = await fetch(LOG_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+      },
+      body: JSON.stringify(payload),
+    });
+
+    return await res.json();
+  } catch (err) {
+    console.log("error");
+  }
+};
